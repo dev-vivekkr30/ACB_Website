3c599187285dacd851daea2f7a2468f624503c9a
dev-vivekkr30 <dev.vivekkr30@gmail.com>
2024-02-09 10:54:16 +0530

    added-owl-carousel

--- JS/main.js
+++ JS/main.js
@@ -1,27 +1,16 @@
-$(document).ready(function () {
-    (function ($) {
-        $('.brand-carousel').owlCarousel({
-            loop:true,
-            margin:10,
-            center:false,
-            autoplayTimeout:4000,
-            autoplay:false,
-            dots:false,
-            nav:true,
-            responsive:{
-                0:{
-                    items:1
-                },
-                600:{
-                    items:2
-                },
-                1000:{
-                    items:2
-                }
-            }
-         })
-    })(jQuery);
-});
-
-
-
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
